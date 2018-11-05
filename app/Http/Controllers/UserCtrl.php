868a2db6019d705e3e28d165f49b83c74d566e43
feyex <feyisewaawotedu@gmail.com>
2018-11-05 15:47:14 +0100

    fix:user signup (backend)

---
+++ app/Http/Controllers/UserCtrl.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\user_reg;
+use illuminate\Http\Request;
+
+class UserCtrl extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function register(Request $request) 
+    {
+        $response = 
+        $this->validate(
+            $request, [
+                'username' => 'required',
+                'email' => 'required|email|unique:user',
+                'password' => 'required'
+            ]
+            );
+        
+        $hasher = app()->make('hash');
+        $user = new user_reg();
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password =$hasher->make($request->password) ;
+        $user->save();
+
+        if ($user->save () )
+        {
+            $response = response ()->json(
+                [
+                    'response' => [
+                        'created' =>true,
+                        'user_id' =>$user->id
+                    ]
+                    ],201
+            );
+        }
+        return $response;
+    }
+}
---
+++ app/user_reg.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class user_reg extends Model
+{
+//use Authenticatable, Authorizable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $table = 'user';
+    protected $fillable = [
+        'username', 
+        'email', 
+        'password', 
+       
+      
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+  
+    // public function products(){
+    //     return $this->belongsTo('App\Product');
+    //  }
+    
+}
